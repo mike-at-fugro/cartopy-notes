6c62a890aced9c2c3e28b59b61fba05cb8830e1a
mike-at-fugro <m.failey@fugro.com>
2024-08-21 15:36:00 +0100

    new york in lower resolution

--- further regional maps.py
+++ further regional maps.py
@@ -48,7 +48,7 @@ plt.show()
 """
     Stinky New York
 
-    low resolution
+    lower resolution
 
 """
 latN = 45.2
@@ -61,12 +61,48 @@ projLccNY = ccrs.LambertConformal(central_longitude=cLon, central_latitude=cLat)
 fig = plt.figure(figsize=(15, 10))
 ax = plt.subplot(1, 1, 1, projection=projLccNY)
 ax.set_extent([lonW, lonE, latS, latN], crs=proj_PC)
-ax.set_facecolor(cfeature.COLORS['water'])
-ax.add_feature(cfeature.LAND)
-ax.add_feature(cfeature.COASTLINE)
-ax.add_feature(cfeature.BORDERS, linestyle='--')
-ax.add_feature(cfeature.LAKES, alpha=0.5)
-ax.add_feature(cfeature.STATES)
-ax.add_feature(cfeature.RIVERS)
-ax.set_title('New York and Vicinity')
+
+"""
+    low-res shapefile objects
+
+        The features with names such as cfeature.LAND, cfeature.OCEAN, are higher-resolution (10m) shapefiles from the Naturalearth repository.
+        Lower resolution shapefiles (50m, 110m) can be used by using the cfeature.NaturalEarthFeature method as illustrated below.
+"""
+
+resolution = '110m'
+
+land_mask = cfeature.NaturalEarthFeature(
+    'physical',
+    'land',
+    scale=resolution,
+    edgecolor='face',
+    facecolor=cfeature.COLORS['land'],
+)
+sea_mask = cfeature.NaturalEarthFeature(
+    'physical',
+    'ocean',
+    scale=resolution,
+    edgecolor='face',
+    facecolor=cfeature.COLORS['water'],
+)
+lake_mask = cfeature.NaturalEarthFeature(
+    'physical',
+    'lakes',
+    scale=resolution,
+    edgecolor='face',
+    facecolor=cfeature.COLORS['water'],
+)
+state_borders = cfeature.NaturalEarthFeature(
+    category='cultural',
+    name='admin_1_states_provinces_lakes',
+    scale=resolution,
+    facecolor='none',
+)
+
+ax.add_feature(land_mask)
+ax.add_feature(sea_mask)
+ax.add_feature(lake_mask)
+ax.add_feature(state_borders, linestyle='solid', edgecolor='black')
+ax.set_title('New York and Vicinity; lower resolution')
+
 plt.show()
