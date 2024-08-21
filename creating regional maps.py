2f1433459d8f534c577af9c8f82584b525cf6aba
mike-at-fugro <m.failey@fugro.com>
2024-08-21 14:45:52 +0100

    added stereographic projection

--- creating regional maps.py
+++ creating regional maps.py
@@ -30,8 +30,9 @@ lonW = -140
 lonE = -40
 latS = 15
 latN = 65
+lims = [lonW,lonE,latS,latN]
 
-ax.set_extent([lonW,lonE,latS,latN], crs=proj_PC)
+ax.set_extent(lims, crs=proj_PC)
 
 """
     adding features
@@ -45,4 +46,24 @@ ax.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')
 
 plt.show()
 
-#Stereographic version NEXT
+
+"""
+    Stereographic Projection - displays polar regions more accurately
+
+"""
+#define central longitude and latitude
+cLon = (lonW + lonE) / 2
+cLat = (latN + latS) / 2
+
+proj_Stereo = ccrs.Stereographic(central_longitude=cLon, central_latitude=cLat)
+
+fig2 = fig
+ax2 = plt.subplot(1, 1, 1, projection=proj_Stereo)
+ax2.set_title("Stereographic")
+gl2 = ax2.gridlines(draw_labels=True, linewidth=2, color='gray', alpha=0.5, linestyle='--')
+
+ax2.set_extent(lims, crs=proj_PC)
+ax2.coastlines(resolution=res, color='black')
+ax2.add_feature(cfeature.STATES, linewidth=0.3, edgecolor='brown')
+ax2.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')
+plt.show()
