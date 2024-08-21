1710a20e6f7136d580a1f7cf399c9f87ff5a0237
mike-at-fugro <m.failey@fugro.com>
2024-08-21 14:52:24 +0100

    added lambert conformal projection

--- creating regional maps.py
+++ creating regional maps.py
@@ -67,3 +67,20 @@ ax2.coastlines(resolution=res, color='black')
 ax2.add_feature(cfeature.STATES, linewidth=0.3, edgecolor='brown')
 ax2.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')
 plt.show()
+
+
+"""
+    Lambert Conformal Projection
+
+"""
+proj_Lcc = ccrs.LambertConformal(central_longitude=cLon, central_latitude=cLat)
+
+fig3 = fig
+ax3 = plt.subplot(1, 1, 1, projection=proj_Lcc)
+ax3.set_title('Lambert Conformal')
+gl3 = ax3.gridlines(draw_labels=True, linewidth=2, color='gray', alpha=0.5, linestyle='--')
+ax3.set_extent(lims, crs=proj_PC)
+ax3.coastlines(resolution=res, color='black')
+ax3.add_feature(cfeature.STATES, linewidth=0.3, edgecolor='brown')
+ax3.add_feature(cfeature.BORDERS, linewidth=0.5, edgecolor='blue')
+plt.show()
